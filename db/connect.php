628355c5821f545e727e76c7b77aa04b23397a43
PranavChendur T K <pranav@pricpro.in>
2016-10-19 23:59:29 +0530

    changed database

--- db/connect.php
+++ db/connect.php
@@ -1,10 +1,13 @@
 <?php
-   $host        = "host=localhost";
-   $port        = "port=5432";
-   $dbname      = "dbname=postgres";
-   $credentials = "user=postgres password=pranav#14";
+   // $host        = "host=localhost";
+   // $port        = "port=5432";
+   // $dbname      = "dbname=postgres";
+   // $credentials = "user=postgres password=pranav#14";
 
-   $db = pg_connect( "$host $port $dbname $credentials"  );
+   // $db = pg_connect( "$host $port $dbname $credentials"  );
+
+   $db = pg_connect( "dbname=ddcdnokrmhd5m4 host=ec2-50-19-222-159.compute-1.amazonaws.com port=5432 user=tkrarhvbsfvfqa password=4Jddi5Tct6BBk8u65arzmUhpNQ sslmode=require"  );
+   
    if(!$db){
       echo "Error : Unable to open database\n";
    } else {
